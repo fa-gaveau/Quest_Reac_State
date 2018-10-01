97682f7589eee7f278ae5895adee85ac8321d530
GAVEAU Francois-Axel <gaveau.francois-axel@orange.fr>
2018-10-01 17:28:27 +0200

    QuestSolution

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Lamp from "./Lamp";
-import Quotes from "./Quotes"
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      working: true
+    };
+  }
+  handleClick = () => {
+    this.setState({ working: !this.state.working });
+    let a = document.getElementById('App-logo');
+    
+    if (a.className=="App-logo-1")
+    {
+      a.className="App-logo";
+      a.style.border='0px black solid';
+    }
+
+    else
+    {
+      a.className="App-logo-1";
+      a.style.border='10px black solid';
+    }
+  };
   render() {
+    const working = this.state.working ? 'Homer bosse dur' : 'Va bosser feignasse';
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Simpsons Quotes</h1>
+          <img src={logo} className="App-logo" id="App-logo" alt="logo" />
         </header>
-        <Lamp on />
-        <Lamp />
-        <Quotes />
+        <button
+          onClick={this.handleClick} 
+          className={App}
+        >
+          {working.toUpperCase()}
+        </button>
       </div>
     );
   }
